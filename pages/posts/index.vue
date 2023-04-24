c7dcef7cdb7ae29e4c185ba95677daa6f181c353
Philip <philip.alexander.hassialis@gmail.com>
2023-04-24 09:18:20 +0300

    Exercise 2

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -11,6 +11,28 @@ import PostList from "@/components/Posts/PostList.vue";
 export default {
   components: {
     PostList,
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "Post 1",
+            previewText:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nunc, nec",
+            thumbnail: "https://picsum.photos/200/300?cache=1",
+          },
+          {
+            id: "2",
+            title: "Post 2",
+            previewText:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nunc, nec",
+            thumbnail: "https://picsum.photos/200/300?cache=2",
+          },
+        ],
+      });
+    }, 1500);
   },
 };
 </script>
