194d4f38487a3d6d4493b5490afef6f5452e2e97
Philip <philip.alexander.hassialis@gmail.com>
2023-04-25 09:17:12 +0300

    using promises in asyncData

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -12,27 +12,33 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "Post 1",
-            previewText:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nunc, nec",
-            thumbnail: "https://picsum.photos/200/300?cache=1",
-          },
-          {
-            id: "2",
-            title: "Post 2",
-            previewText:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nunc, nec",
-            thumbnail: "https://picsum.photos/200/300?cache=2",
-          },
-        ],
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "Post 1",
+              previewText:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nunc, nec",
+              thumbnail: "https://picsum.photos/200/300?cache=1",
+            },
+            {
+              id: "2",
+              title: "Post 2",
+              previewText:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nunc, nec",
+              thumbnail: "https://picsum.photos/200/300?cache=2",
+            },
+          ],
+        });
+      }, 1500);
+    })
+      .then((data) => data)
+      .catch((err) => {
+        context.error(new Error());
       });
-    }, 1500);
   },
 };
 </script>
